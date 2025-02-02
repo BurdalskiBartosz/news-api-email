2d53fd4b8e0a5ff58d1092f87fadfaa5faa77af1
Bartosz <bartosz.burdalski@gmail.com>
2025-02-02 09:33:59 +0100

    Create list of news and send email

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from dotenv import load_dotenv
 import os
 
+from send_email import send_email
+
 load_dotenv()
 
 api_key = os.getenv("API_KEY")
@@ -10,5 +12,10 @@ url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-01-02&sortBy=publish
 request = requests.get(url)
 content = request.json()
 
-for item in content["articles"]:
-    print(item['title'])
+message = ""
+body = ""
+for article in content["articles"]:
+    body = body + article["title"] + "\n" + article["description"] + 2* "\n"
+
+body = body.encode('utf-8')
+send_email(body)

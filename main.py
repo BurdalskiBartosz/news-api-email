6dba3de8a6d4035b7c73e9c86638757f5e2ce988
Bartosz <bartosz.burdalski@gmail.com>
2025-02-02 13:14:51 +0100

    Improve sending emails

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import os
 from send_email import send_email
 
 load_dotenv()
+topic = "tesla"
 
 api_key = os.getenv("API_KEY")
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-01-02&sortBy=publishedAt&apiKey={api_key}"
+url = ("https://newsapi.org/v2/everything"
+       f"?q={topic}&from=2025-01-02&sortBy=publishedAt&apiKey={api_key}&language=en")
 
 request = requests.get(url)
 content = request.json()
 
 message = ""
 body = ""
-for article in content["articles"]:
-    body = body + article["title"] + "\n" + article["description"] + 2* "\n"
+for article in content["articles"][:20]:
+    if article["title"] is not None and article["description"] is not None:
+        body = (body + article["title"] + "\n"
+               + article["description"]
+               + "\n" + article["url"] + 2*"\n")
 
-body = body.encode('utf-8')
-send_email(body)
+body = "Subject: Today's news" + "\n" + body
+send_email(body.encode('utf-8'))
